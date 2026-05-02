002c30a6d35e9dc25c1ee2aecd0b6d9e408f2989
Chace Ethan <nzabandoraisiac@gmail.com>
2026-05-01 23:21:28 -1000

    chore: secure env, fix frontend config, production ready

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "VITE_");
   const apiUrl = env.VITE_API_URL;
 
   return {
